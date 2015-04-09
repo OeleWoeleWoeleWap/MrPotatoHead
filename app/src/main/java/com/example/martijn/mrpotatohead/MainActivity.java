48f652805a9700ecd9e8b48a1eefc8610d152d0f
Martijn <martijn.github@gmail.com>
2015-04-09 21:02:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/martijn/mrpotatohead/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.martijn.mrpotatohead;
+
+import android.media.Image;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void toggleVisibility(View view) {
+        String partName = view.getResources().getResourceName(view.getId()).split("checkbox_")[1];
+        int partID = getResources().getIdentifier(partName, "id", "com.example.martijn.mrpotatohead");
+        ImageView part = (ImageView) findViewById(partID);
+        toggleElementVisibility(part);
+    }
+
+    private void toggleElementVisibility(ImageView part){
+        if(part.getVisibility() == 4) part.setVisibility(part.VISIBLE);
+        else if (part.getVisibility() == 0) part.setVisibility(part.INVISIBLE);
+    }
+
+}
